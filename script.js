d06f85b7aeb07eeb673be154ec277573830ad990
Frox <112247156+iFxrr@users.noreply.github.com>
2023-11-16 12:37:24 +0200

    Rihla

--- script.js
+++ script.js
@@ -1,62 +1,87 @@
+// Track the previous scroll position for header visibility changes
 var prevScrollPos = $(window).scrollTop();
 
+// Handle header visibility on scroll
 $(window).scroll(function () {
   var currentScrollPos = $(window).scrollTop();
   var header = $("header");
 
+  // Check scroll direction to determine header class changes
   if (prevScrollPos > currentScrollPos) {
-    header.removeClass("header-hidden");
-    header.addClass("header-on");
+    header.removeClass("header-hidden").addClass("header-on");
   } else {
-    header.addClass("header-hidden");
-    header.removeClass("header-on");
+    header.addClass("header-hidden").removeClass("header-on");
   }
 
+  // Change header background on scroll
   if (window.scrollY > 0) {
-    header.addClass("header-bg"); // Add the class to hide and change background
+    header.addClass("header-bg");
   } else {
-    header.removeClass("header-bg"); // Remove the class to show and restore background
+    header.removeClass("header-bg");
   }
 
+  // Update previous scroll position
   prevScrollPos = currentScrollPos;
 });
 
-//collapsed nav-bar
-
+// Handle collapsed navigation bar
 const navBarIcon = $(".collapsed-nav");
 const phoneNavBar = $(".phone-nav");
 const closeNavBar = $(".close-ico");
 
-navBarIcon.on("click",function (){
-phoneNavBar.addClass("show-nav");
+// Show mobile navigation on icon click
+navBarIcon.on("click", function () {
+  phoneNavBar.addClass("show-nav");
 });
 
+// Hide mobile navigation on close icon click
 closeNavBar.on("click", function () {
   phoneNavBar.removeClass("show-nav");
 });
 
-//footer section
+// Handle dropdown functionality in the footer section
 $(".dropdown").click(function () {
   $(this).find(".collapse").toggleClass("expand");
 });
 
-/* carousel cards */
+// Animate sections on scroll
+const sections = $("section");
 
-// Select the carousel element with the jQuery selector
+function reveal() {
+  var windowHeight = $(window).innerHeight();
+  var windowBottom = $(window).scrollTop() + windowHeight;
+  var revealPoint = windowBottom - 100;
+
+  sections.each(function () {
+    var offset = $(this).offset().top;
+    var sectionBottom = offset + $(this).outerHeight();
+
+    if (offset <= revealPoint && sectionBottom > $(window).scrollTop()) {
+      $(this).addClass("show-animate");
+    } else {
+      $(this).removeClass("show-animate");
+    }
+  });
+}
+
+$(window).on("scroll", reveal);
+
+// Carousel cards functionality
 const carousel = $("#posts .container .carousel");
 const arrows = $("#posts .arrow");
 const firstCard = $("#posts .activity").outerWidth(true);
 const carouselChildren = carousel.children().toArray();
 
-// Initialize variables to track dragging state and position
+// Initialize variables for dragging state and position
 let isDragging = false,
   startX,
   startScrollLeft,
   timeoutId;
 
+// Calculate the number of cards per view
 let cardPerView = Math.round(carousel.outerWidth(true) / firstCard);
 
-// Prepend cards at the beginning and append cards at the end of the carousel
+// Prepend and append cards for infinite scrolling effect
 carouselChildren
   .slice(-cardPerView)
   .reverse()
@@ -68,6 +93,7 @@ carouselChildren.slice(0, cardPerView).forEach(function (card) {
   carousel.append($(card).prop("outerHTML"));
 });
 
+// Event handlers for arrow clicks
 arrows.each(function () {
   $(this).on("click", function (e) {
     const arrow = $(this);
@@ -78,7 +104,7 @@ arrows.each(function () {
   });
 });
 
-// Function to start dragging
+// Functions for dragging, infinite scrolling, and auto sliding
 const dragStart = function (e) {
   isDragging = true;
   carousel.addClass("dragging");
@@ -86,23 +112,19 @@ const dragStart = function (e) {
   startScrollLeft = carousel.scrollLeft();
 };
 
-// Function to stop dragging
 const dragStop = function () {
   isDragging = false;
   carousel.removeClass("dragging");
 };
 
-// Function to handle dragging
 const dragging = function (e) {
-  if (!isDragging) return; // Don't proceed if not dragging
+  if (!isDragging) return;
   carousel.scrollLeft(startScrollLeft - (e.pageX - startX));
 };
 
-// Function for infinite scrolling
 const infiniteScroll = function () {
-  const carousel = $("#posts .container .carousel");
   const carouselScrollLeft = carousel.scrollLeft();
-  const carouselScrollWidth = carousel[0].scrollWidth; 
+  const carouselScrollWidth = carousel[0].scrollWidth;
   const carouselWidth = carousel.outerWidth();
 
   if (carouselScrollLeft === 0) {
@@ -131,9 +153,10 @@ const autoSlide = () => {
   );
 };
 
+// Initial auto slide
 autoSlide();
 
-// Attach event handlers
+// Attach event handlers for dragging and resizing
 carousel.on("mousemove", dragging);
 $(document).on("mouseup", dragStop);
 carousel.on("mousedown", dragStart);
@@ -141,8 +164,9 @@ carousel.on("scroll", infiniteScroll);
 carousel.on("mouseenter", () => clearTimeout(timeoutId));
 carousel.on("mouseleave", autoSlide);
 
+// Function to change inner HTML based on window width
 function changeInnerHtml() {
-  var element = $(".activity.large h3"); 
+  var element = $(".activity.large h3");
 
   if ($(window).width() < 723) {
     element.text("استمتع بشواطئ البحر في الغردقة");
@@ -151,6 +175,6 @@ function changeInnerHtml() {
   }
 }
 
+// Call the function on page load and window resize
 changeInnerHtml();
-
 $(window).resize(changeInnerHtml);
